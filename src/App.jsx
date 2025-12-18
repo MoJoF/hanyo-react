e68a23bd8b3ea4fa8034b68271baaa17ab2ec990
MoJoF <mojo.13081998@gmail.com>
2025-12-18 21:15:04 +0200

    Доделка условий использования (/terms)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import Policy from "./pages/Policy"
+import Terms from "./pages/Terms"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/policy" element={<Policy />} />
+      <Route path="/terms" element={<Terms />} />
 
       {/* 404 */}
       <Route path="*" element={<NotFound />} />
